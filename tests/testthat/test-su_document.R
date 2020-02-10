dacd23e37638bb90eb2bfc13f2f519ba89a283fe
Tom Jemmett <thomas.jemmett@nhs.net>
2020-02-10 14:22:42 +0000

    adds test for su_document

---
+++ tests/testthat/test-su_document.R
@@ -0,0 +1,34 @@
+library(testthat)
+library(mockery)
+
+context("su_document")
+
+test_that("calls rmarkdown::word_document", {
+  m <- mock("word_document")
+  s <- mock("su_template.docx")
+
+  stub(su_document, "rmarkdown::word_document", m)
+  stub(su_document, "system.file", s)
+
+  su_document()
+
+  expect_called(s, 1)
+  expect_args(s, 1,
+              "rmarkdown/templates/su-document/Strategy_Unit_Template.docx",
+              package = "StrategyUnitTheme")
+
+  expect_called(m, 1)
+  expect_args(m, 1,
+              toc = FALSE,
+              toc_depth = 3,
+              fig_width = 5,
+              fig_height = 4,
+              fig_caption = TRUE,
+              df_print = "default",
+              smart = TRUE,
+              highlight = "default",
+              reference_docx = "su_template.docx",
+              keep_md = FALSE,
+              md_extensions = NULL,
+              pandoc_args = NULL)
+})
